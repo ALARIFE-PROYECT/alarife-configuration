ecba5bec0a76914a5ee58863a654c810152c35b5
Soria Garcia, Jose Eduardo <jesoria@indra.es>
2026-04-09 17:00:51 +0200

    feat: comments

--- src/models/ConfigLoader.ts
+++ src/models/ConfigLoader.ts
@@ -17,38 +17,53 @@ export class EnvConfigurationLoader implements ConfigurationLoader {
     const environment = state.getProperty(NODE_ENV);
     if (environment) {
       const envConfig = this.loadEnvFile(`.env.${environment}`);
-      Object.keys(envConfig).forEach((key) => {
-        const appConfig = appConfigurationMap.find((config) => config.envKey === key);
 
-        state.setProperty({ argKey: appConfig?.argKey, envKey: key, value: envConfig[key] });
-      });
+      /**
+       * @alarife/commander sera quien configure la relacion entre argv y env
+       */
+
+      // Object.keys(envConfig).forEach((key) => {
+      //   const appConfig = appConfigurationMap.find((config) => config.envKey === key);
+
+      //   state.setProperty({ argKey: appConfig?.argKey, envKey: key, value: envConfig[key] });
+      // });
     }
   }
 }
 
 export class ArgvConfigurationLoader implements ConfigurationLoader {
   load(state: ConfigurationState): void {
-    const args = process.argv.slice(2);
-    appConfigurationMap.forEach(({ argKey, envKey, defaultValue, argvType }) => {
-      const argvElement = args.find((arg) => arg.startsWith(`--${argKey}`));
-      if (argvElement) {
-        if (argvType === 'boolean') {
-          state.setProperty({ argKey, envKey, value: true });
-        } else {
-          const [, value] = argvElement.split('=');
-          state.setProperty({ argKey, envKey, value: value ?? defaultValue });
-        }
-      }
-    });
+    /**
+     * No existe mapeo de configuraciones todo estara mapeado por @alarife/commander
+     * Lo que este retorne es lo correcto y se llevara al ConfigurationState
+     */
+
+
+    // const args = process.argv.slice(2);
+    // appConfigurationMap.forEach(({ argKey, envKey, defaultValue, argvType }) => {
+    //   const argvElement = args.find((arg) => arg.startsWith(`--${argKey}`));
+    //   if (argvElement) {
+    //     if (argvType === 'boolean') {
+    //       state.setProperty({ argKey, envKey, value: true });
+    //     } else {
+    //       const [, value] = argvElement.split('=');
+    //       state.setProperty({ argKey, envKey, value: value ?? defaultValue });
+    //     }
+    //   }
+    // });
   }
 }
 
 export class DefaultConfigurationLoader implements ConfigurationLoader {
   load(state: ConfigurationState): void {
-    appConfigurationMap.forEach(({ argKey, envKey, defaultValue }) => {
-      if (!!defaultValue) {
-        state.setProperty({ argKey, envKey, value: defaultValue });
-      }
-    });
+    /**
+     * @alarife/commander tendra configurado los valores por defecto, solo hay que leer los ARGV
+     */
+
+    // appConfigurationMap.forEach(({ argKey, envKey, defaultValue }) => {
+    //   if (!!defaultValue) {
+    //     state.setProperty({ argKey, envKey, value: defaultValue });
+    //   }
+    // });
   }
 }
--- src/models/ConfigPostLoader.ts
+++ src/models/ConfigPostLoader.ts
@@ -2,52 +2,52 @@ import crypto from 'crypto';
 
 import { ConfigurationLoader } from './ConfigLoader';
 import { ConfigurationState } from './ConfigurationState';
-import { ENCRYPT_KEY } from './ConfigurationEnvKeys';
+// import { ENCRYPT_KEY } from './ConfigurationEnvKeys';
 
-const ALGORITHM = 'aes-256-gcm';
-const IV_LENGTH = 16;
-const AUTH_TAG_LENGTH = 16;
-const CIPHER_PREFIX = 'ENC[';
+// const ALGORITHM = 'aes-256-gcm';
+// const IV_LENGTH = 16;
+// const AUTH_TAG_LENGTH = 16;
+// const CIPHER_PREFIX = 'ENC[';
 
 export interface ConfigurationPostLoader extends ConfigurationLoader {}
 
 export class SecureConfigurationPostLoader implements ConfigurationPostLoader {
-  private derivedKey?: Uint8Array;
+  // private derivedKey?: Uint8Array;
 
-  private createDerivedKey(password: string): void {
-    this.derivedKey = new Uint8Array(crypto.scryptSync(password, 'salt', 32));
-  }
+  // private createDerivedKey(password: string): void {
+  //   this.derivedKey = new Uint8Array(crypto.scryptSync(password, 'salt', 32));
+  // }
 
-  private decrypt(cipherValue: string): string {
-    const payload = Buffer.from(cipherValue.slice(CIPHER_PREFIX.length), 'hex');
+  // private decrypt(cipherValue: string): string {
+  //   const payload = Buffer.from(cipherValue.slice(CIPHER_PREFIX.length), 'hex');
 
-    const iv = new Uint8Array(payload.subarray(0, IV_LENGTH));
-    const authTag = new Uint8Array(payload.subarray(IV_LENGTH, IV_LENGTH + AUTH_TAG_LENGTH));
-    const encrypted = new Uint8Array(payload.subarray(IV_LENGTH + AUTH_TAG_LENGTH));
+  //   const iv = new Uint8Array(payload.subarray(0, IV_LENGTH));
+  //   const authTag = new Uint8Array(payload.subarray(IV_LENGTH, IV_LENGTH + AUTH_TAG_LENGTH));
+  //   const encrypted = new Uint8Array(payload.subarray(IV_LENGTH + AUTH_TAG_LENGTH));
 
-    const decipher = crypto.createDecipheriv(ALGORITHM, this.derivedKey!, iv);
-    decipher.setAuthTag(authTag);
+  //   const decipher = crypto.createDecipheriv(ALGORITHM, this.derivedKey!, iv);
+  //   decipher.setAuthTag(authTag);
 
-    let decrypted = decipher.update(encrypted, undefined, 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
-  }
+  //   let decrypted = decipher.update(encrypted, undefined, 'utf8');
+  //   decrypted += decipher.final('utf8');
+  //   return decrypted;
+  // }
 
   load(state: ConfigurationState): void {
-    console.log("🚀 ~ SecureConfigurationPostLoader ~ load ~ state:", state)
-    const encryptKey = 'age12hmtsadz37de2q85tvzvxyqs64h65fm8hlkcy8w93a4m6uglqcmq2rgerk';
-    if (!encryptKey) {
-      return;
-    }
+    // console.log("🚀 ~ SecureConfigurationPostLoader ~ load ~ state:", state)
+    // const encryptKey = 'age12hmtsadz37de2q85tvzvxyqs64h65fm8hlkcy8w93a4m6uglqcmq2rgerk';
+    // if (!encryptKey) {
+    //   return;
+    // }
 
-    this.createDerivedKey(encryptKey);
+    // this.createDerivedKey(encryptKey);
 
-    state.forEach((property) => {
-      console.log("🚀 ~ SecureConfigurationPostLoader ~ load ~ property:", property)
-      if (property.value && typeof property.value === 'string' && property.value.includes(CIPHER_PREFIX)) {
-        property.value = this.decrypt(property.value);
-        console.log("🚀 ~ SecureConfigurationPostLoader ~ load ~ property.value:", property.value)
-      }
-    });
+    // state.forEach((property) => {
+    //   console.log("🚀 ~ SecureConfigurationPostLoader ~ load ~ property:", property)
+    //   if (property.value && typeof property.value === 'string' && property.value.includes(CIPHER_PREFIX)) {
+    //     property.value = this.decrypt(property.value);
+    //     console.log("🚀 ~ SecureConfigurationPostLoader ~ load ~ property.value:", property.value)
+    //   }
+    // });
   }
 }
